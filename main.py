3496b760a96672825bc57bb141cb5ae5d3becfe7
Ricardo Balsamo <93337947+ricardobal@users.noreply.github.com>
2024-08-19 18:49:56 -0300

    Agrego el modulo de la parte de datos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from modules.api import get_data_from_api
+from modules.configdb import grabar_archivo
 
 
 
@@ -7,13 +8,19 @@ def main():
     url = "https://api.openf1.org/v1/sessions"
     data = get_data_from_api(url, 'latest')
     last_session = data["session_key"]
-    ## print(last_session)
+    print(last_session)
 
-
+    ## Descargo los tiempos de la ultima session
     url = "https://api.openf1.org/v1/laps"
     data = get_data_from_api(url, last_session)
     print(data)
 
+    ## Defino las variables de DB
+    file_path = 'f1laps.parquet'
+    table_name = 'laps'
+
+    ## grabo 
+    grabar_archivo(file_path, table_name)
 
 if __name__ == "__main__":
     main()
